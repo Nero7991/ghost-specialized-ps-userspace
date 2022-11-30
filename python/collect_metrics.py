7a1f8ae707f88d7483e9a127c477820a038f5317
Oren Collaco <orencollaco97@gmail.com>
2022-11-30 04:14:16 +0000

    Added spec_file parsing and writing metrics to csv file in metrics folder

--- python/collect_metrics.py
+++ python/collect_metrics.py
@@ -1,64 +1,85 @@
 from bcc import BPF
 from os import system
 import time
+import sys
 
 # define BPF program
 prog = """
-int hello(void *ctx) {
-    bpf_trace_printk("hello");
+int syscall_event(void *ctx) {
+    bpf_trace_printk("syscall");
     return 0;
 }
 """
 
-# load BPF program
-b = BPF(text=prog)
-c = BPF(text=prog)
-b.attach_kprobe(event=b.get_syscall_fnname("write"), fn_name="hello")
-c.attach_kprobe(event=b.get_syscall_fnname("read"), fn_name="hello")
+syscalls = []
+thresholds = []
+calls = []
+progs = []
+name = ""
 
-# header
-print("%-18s %-16s %-6s %s" % ("TIME(s)", "COMM", "PID", "MESSAGE"))
-read_calls = 0
-write_calls = 0
-current_time = 0
+# Read spec file to determine the syscalls of interest
+try:
+    f = open("../spec_files/simple_exp_spec.csv")
+    lines = f.readlines()
+    if(len(lines) > 0):
+        for i, line in enumerate(lines):
+            if(i == 0):
+                name = line.split(',')[1]
+                name = name.replace('\n', "")
+            else:
+                if(len(line) > 0):
+                    syscalls.append(line.split(',')[0])
+                    thresholds.append(int(line.split(',')[1]))
+                    calls.append(0)
+
+except Exception as e:
+    print(e)
+    
+# Print the syscalls and thresholds read from the spec file
+print("name = " + name)
+print("syscalls = ")
+print(syscalls)
+print("thresholds = ")
+print(thresholds)
+
+# load BPF program to monitor each syscall in the spec file
+for i, syscall in enumerate(syscalls):
+    progs.append(BPF(text=prog.replace("syscall", syscall)))
+    progs[i].attach_kprobe(event=progs[i].get_syscall_fnname(syscall), fn_name=(syscall + "_event"))
+    # print(progs[i])
+
+last_time = 0
 # format output
-while 1:
-    try:
-        (task, pid, cpu, flags, ts, msg) = b.trace_fields()
-    except ValueError:
-        continue
-    
-    try:
-        f = open("/proc/" + str(pid) + "/cmdline")
-        process_location = f.readlines()
-        if(len(process_location) > 0):
-            if("mysql" in process_location[0]):
-                read_calls+=1
-                print(str(i) + " mysql %-18.9f %-16s %-6d %s" % (ts, task, pid, msg))
-
-    except Exception as e:
-        # print(e)
-        # print("pid = " + str(pid))
-        continue
-    try:
-        (task, pid, cpu, flags, ts, msg) = b.trace_fields()
-    except ValueError:
-        continue
-    
-    try:
-        f = open("/proc/" + str(pid) + "/cmdline")
-        process_location = f.readlines()
-        if(len(process_location) > 0):
-            if("mysql" in process_location[0]):
-                write_calls+=1
-                print(str(i) + " mysql %-18.9f %-16s %-6d %s" % (ts, task, pid, msg))
-    except Exception as e:
-        # print(e)
-        # print("pid = " + str(pid))
-        continue
-    
-    if(time.time() - current_time > 1):
-        print("sys_read calls = " + str(read_calls) + " per sec, sys_write calls = " + str(write_calls) + " per sec") 
-        read_calls = 0
-        write_calls = 0
-        current_time = time.time()      
+try:
+    while 1:
+        for i, prog in enumerate(progs):
+            try:
+                #get trace fields from the bpf program
+                (task, pid, cpu, flags, ts, msg) = prog.trace_fields()
+                f = open("/proc/" + str(pid) + "/cmdline")
+                process_location = f.readlines()
+                if(len(process_location) > 0):
+                    # check if this trace belongs to process of interest
+                    if((name in process_location[0]) and syscalls[i] in msg):
+                        calls[i]+=1
+            except Exception as e:
+                #print(e)
+                continue
+        
+        if(time.time() - last_time > 1):
+            textbuf = name + "\n"
+            for i, syscall in enumerate(syscalls):
+                if(calls[i] > thresholds[i]):
+                    textbuf += syscall + "," + str(calls[i]) + ",1\n"
+                else:
+                    textbuf += syscall + "," + str(calls[i]) + ",0\n"
+                calls[i] = 0
+            print(textbuf)
+            f = open("../metrics/"+ name + ".csv", "w")
+            f.write(textbuf)
+            f.close()
+            last_time = time.time()
+except KeyboardInterrupt:
+    pass
+    print("Exiting...") 
+    sys.exit()   
